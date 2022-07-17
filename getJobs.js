fd6ce76901c77b6d4d51d96ca9e8ec20bf09229f
Sarobidy-23 <100074450+Sarobidy-23@users.noreply.github.com>
2022-07-17 17:22:03 +0300

    get all jobs

---
+++ getJobs.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+const core = require('@actions/core');
+const axios = require('axios');
+
+const content = async (listOfJobs, numPage) => {
+    const browser = await puppeteer.launch({
+        headless: true
+    }) ;
+    const page = await browser.newPage();
+    try {
+        await page.goto(`https://www.portaljob-madagascar.com/emploi/liste/secteur/informatique-web/page/${numPage}`, {timeout: 0, waitUntil: 'networkidle2'});
+            const jobs = await page.evaluate((listOfJobs)=>{
+                let elements = document.querySelectorAll('body > section.col2_max_min > div > div.max > article');
+                for ( let element of elements){
+                    let condition = "ok";
+                    for(let info of listOfJobs){
+                        if(info.poste.toString() == element.querySelector('h3').textContent.toString()){
+                            condition = "no";
+                            break;
+                        };
+                    };
+                    
+                    if(condition == "ok"){
+                            listOfJobs.push({
+                                poste : element.querySelector('h3').textContent ,
+                                societe : element.querySelector('h4').textContent,
+                                contrat : element.querySelector('h5').textContent,
+                                description : element.querySelector('a.description ').text,
+                                date: element.querySelector('aside.date_annonce > div.date').textContent,
+                                url: element.querySelector('aside.contenu_annonce > h3 > a').href
+                            });
+                    };
+                };    
+            return listOfJobs
+            }, listOfJobs)
+        core.setOutput('joblist', jobs);
+   } catch (error) {
+        console.log(error);
+   }
+    
+    await browser.close();
+};
+
+const promise = axios.get("https://raw.githubusercontent.com/Sarobidy-23/webscraping/main/informatique-web.json");
+    promise
+    .then((response) => {   
+        let result = JSON.stringify(response.data)
+        let listOfJobs = JSON.parse(result)
+        content(listOfJobs, 1)    
+    }).catch((error) => {
+        console.log(error)
+    });
