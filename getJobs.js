a47e5d39e14c83b82a53e3282367e2a66f46113b
Sarobidy-23 <100074450+Sarobidy-23@users.noreply.github.com>
2022-07-21 15:14:27 +0300

    Update getJobs.js

--- getJobs.js
+++ getJobs.js
@@ -9,13 +9,12 @@ const content = async (listOfJobs, numPage) => {
     const page = await browser.newPage();
     try {
         await page.goto(`https://www.portaljob-madagascar.com/emploi/liste/secteur/informatique-web/page/${numPage}`, {timeout: 0, waitUntil: 'networkidle2'});
-        await page.awaitForTimeout(100);
         const jobs = await page.evaluate((listOfJobs)=>{
                 let elements = document.querySelectorAll('body > section.col2_max_min > div > div.max > article');
                 for ( let element of elements){
                     let condition = "ok";
-                    for(let i=0; i<20; i++ ){
-                        if(listOfJobs[i].poste.toString() == element.querySelector('h3').textContent.toString()){
+                    for(let info of listOfJobs){
+                        if(info.poste.toString() == element.querySelector('h3').textContent.toString()){
                             condition = "no";
                             break;
                         };
